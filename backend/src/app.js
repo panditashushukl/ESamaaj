5d18042f72b55cce7f147f03b157f331c4fbe113
panditshukl <panditashushukl@gmail.com>
2025-08-16 21:15:25 +0530

    Refactor API routes to use a versioning prefix, update healthcheck response, and consolidate user and video model definitions

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,19 +29,20 @@ import tweetRouter from "./routes/tweet.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 import videoRouter from "./routes/video.routes.js"
 import commentRouter from "./routes/comment.routes.js"
-import likeRouter from "./routes/like.routes.js"
+import likeRouter from "./routes/likes.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import dashboardRouter from "./routes/dashboard.routes.js"
 
 // Routes Declaration
-app.use("/api/v1/healthcheck", healthcheckRouter)
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/tweets", tweetRouter)
-app.use("/api/v1/subscriptions", subscriptionRouter)
-app.use("/api/v1/videos", videoRouter)
-app.use("/api/v1/comments", commentRouter)
-app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/playlist", playlistRouter)
-app.use("/api/v1/dashboard", dashboardRouter)
+const apiVersion = "/api/v1/"
+app.use(`${apiVersion}healthcheck`, healthcheckRouter)
+app.use(`${apiVersion}users`, userRouter)
+app.use(`${apiVersion}tweets`, tweetRouter)
+app.use(`${apiVersion}subscriptions`, subscriptionRouter)
+app.use(`${apiVersion}videos`, videoRouter)
+app.use(`${apiVersion}comments`, commentRouter)
+app.use(`${apiVersion}likes`, likeRouter)
+app.use(`${apiVersion}playlist`, playlistRouter)
+app.use(`${apiVersion}dashboard`, dashboardRouter)
 
 export default app;
