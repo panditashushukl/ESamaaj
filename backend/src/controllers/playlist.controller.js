a922537dfcb9ff81434f2be0ac99604a8a69405e
panditshukl <panditashushukl@gmail.com>
2025-08-21 23:14:10 +0530

    Implement validation and error handling for adding and removing videos from playlists; ensure proper responses for invalid IDs and existing videos

--- backend/src/controllers/playlist.controller.js
+++ backend/src/controllers/playlist.controller.js
@@ -93,12 +93,64 @@ const getPlaylistById = asyncHandler(async (req, res) => {
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
+
+    if (!playlistId || !videoId || !isValidObjectId(videoId) || !isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Either invalid VideoId or invalid PlaylistId")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if (!playlist) {
+      throw new ApiError(404, "Playlist not found")
+    }
+
+    if (playlist.videos.includes(videoId)) {
+        throw new ApiError(409, "Video already exists in the playlist")
+    }
+
+    playlist.videos.push(videoId)
+    await playlist.save()
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            playlist,
+            "Video Added Successfully"
+        )
+    )
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
-    // TODO: remove video from playlist
+    
+    if (!playlistId || !videoId || !isValidObjectId(videoId) || !isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Either invalid VideoId or invalid PlaylistId")
+    }
 
+    const playlist = await Playlist.findById(playlistId)
+
+    if (!playlist) {
+      throw new ApiError(404, "Playlist not found")
+    }
+
+    if (!playlist.videos.includes(videoId)) {
+        throw new ApiError(409, "Video does not exists in the playlist")
+    }
+
+    playlist.videos.pull(videoId)
+    await playlist.save()
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            playlist,
+            "Video Removed Successfully" 
+        )
+    )
 })
 
 const deletePlaylist = asyncHandler(async (req, res) => {
