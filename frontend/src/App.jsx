8326d0f1f44bae47cc0ecf63cf59232cde5a933d
panditshukl <panditashushukl@gmail.com>
2025-09-09 02:36:28 +0530

    setUp Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div class="flex items-center justify-center h-screen">
+        <p class="text-center text-2xl font-semibold">
+          Test
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
